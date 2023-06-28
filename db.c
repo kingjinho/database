3c55264ef848883aab785b986c9e06877d23d518
kingjinho <jinhoshin319@gmail.com>
2023-06-28 10:26:59 +0900

    initial commit

---
+++ db.c
@@ -0,0 +1,66 @@
+#include <errno.h>
+#include <fcntl.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+typedef struct
+{
+    char *buffer;
+    size_t buffer_length;
+    ssize_t input_length;
+} InputBuffer;
+
+InputBuffer* new_input_buffer() {
+    InputBuffer* input_buffer = (InputBuffer*)malloc(sizeof(InputBuffer));
+    input_buffer->buffer = NULL;
+    input_buffer->buffer_length = 0;
+    input_buffer->input_length = 0;
+
+    return input_buffer;
+}
+
+void print_prompt() {
+    printf("db > ");
+}
+
+//read line of input
+//lineptr: a pointer to a variable used to point to the buffer containing the read line
+//n: a pointer to the variable used to save the size of allocated buffer
+//stream: input stream to read from
+//return: number of bytes read
+ssize_t getline(char **lineptr, size_t *n, FILE *stream);
+
+//read from get line, validate & store data
+void read_input(InputBuffer* input_buffer) {
+    ssize_t bytes_read = 
+        getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
+    
+    if(bytes_read <= 0) {
+        printf("Error reading input\n");
+        exit(EXIT_FAILURE);
+    }
+
+    input_buffer->input_length = bytes_read - 1;
+    input_buffer->buffer[bytes_read - 1] = 0;
+}
+
+
+int main(int argc, char* argv[]) {
+
+    InputBuffer* input_buffer = new_input_buffer();
+    while (true) {
+        print_prompt();
+        read_input(input_buffer);
+
+        if(strcmp(input_buffer->buffer, ".exit") == 0) {
+            close_input_buffer(input_buffer);
+            exit(EXIT_SUCCESS);
+        } else {
+            printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+        }
+    }   
+}
